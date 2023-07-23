3117f81de277017f4d18ee0ca0b2ee1fdd8fa302
fiddle.leaf <shanonps@icloud.com>
2023-07-23 16:39:35 -0400

    Base for luhn form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Luhn Algorithm Validator</h1>
+        < Form />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+export default function Form (params) {
+    const [card, setCard] = useState({
+        cardNumber: ""
+    });
+
+    const handleChange = (e) => {
+        setCard({...card, [e.target.name]: e.target.value})
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(card);
+    }
+
+    const verifyCard = () => {
+        //get current card number
+        let x = card?.cardNumber;
+        console.log(x);
+
+        //split string and get each individual num
+        x = x.split("").map(x => {return Number(x)});
+        console.log(x);
+
+        //create logic for 
+
+    }
+
+    verifyCard(card);
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label name="card-number">Enter card number: </label>
+            <input type="text"
+            name="cardNumber"
+            onChange={handleChange}
+            maxLength={16}></input>
+            <button type="submit">submit!</button>
+        </form>
+    )
+    
+}
