501fe80de424d29b81177e81c641f1f5e6520c3b
fiddle.leaf <shanonps@icloud.com>
2023-08-06 08:58:27 -0400

    Add functionality to button click - flashes color and other minor changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,6 @@ export default function Form (props) {
         status: 'unvalidated'       //'unvalidated`, `validating`, `validated`
     });
 
-    //const [showOutput, setShowOutput] = useState(false);
-
     let validRef = useRef(false);
 
     useEffect(()=>{
@@ -39,6 +37,7 @@ export default function Form (props) {
             }
         }).reverse();   //reverse back to og array order
 
+        //get individual digits of number and add them
         cardNums.join("").split("").forEach(num => {
             sum += Number(num);
         })
@@ -63,6 +62,14 @@ export default function Form (props) {
         });
     }
 
+    const handleMouseDown = (e) => {
+        e.target.style.background = "hsl(333, 90%, 87%)";
+    }
+
+    const handleMouseUp = (e) => {
+        e.target.style.background= "hsl(333, 90%, 90%)";
+    }
+
 
     return (
         <form>
@@ -74,11 +81,21 @@ export default function Form (props) {
                     onChange={handleChange} 
                     value={card.cardNum}
                     placeholder="1234567890123456"></input>
-                <button type="submit" onClick={handleClick}>Submit!</button>
+                <button 
+                    type="submit" 
+                    onClick={handleClick}
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}>Submit!</button>
             </div>
             <div className="output">
                 {card.status === 'validated' &&
-                    }
+                    <label><h2>Result</h2>
+                        <output name="result">
+                            {card.cardNum.length > 2 ? 
+                            <span>Card #{card.cardNum} is {validRef.current ? "valid" : "not valid"}.</span> :
+                            <span>Not a valid card number. Try again with 2 or more digits. </span>}
+                        </output>
+                    </label>}
             </div>
         </form>
     )}
