9f24483bba4106e11265b84fe30dc968b8399886
limetortillachip <shanonpa@icloud.com>
2025-09-04 20:03:21 -0400

    Update footer github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer () {
     return (
         <footer>
             <p className="copyright">
-                <a href="https://github.com/fiddle-leaf" alt="Github page">fiddle-leaf</a> &copy; 2023.
+                <a href="https://github.com/limetortillachip" alt="Github page">limetortillachip</a> &copy; 2023.
             </p>
         </footer>
     )
