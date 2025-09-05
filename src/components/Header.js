86d1db9b153fa2b2aa4b803a7656706b1ff14940
limetortillachip <shanonpa@icloud.com>
2025-09-05 10:00:58 -0400

    Update font for body vs heading text

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ export default function Header() {
     return ( 
         <header>
             <h1>Luhn Algorithm Validator</h1>
-        </header>
+            <p> a checksum formula for numbers/digits used with credit card or administrative numbers.</p>
+        </header> 
      );
 }
