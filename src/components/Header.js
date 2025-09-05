cc71521f2c0e0a82215210e32c803d1801e24ca3
limetortillachip <shanonpa@icloud.com>
2025-09-05 19:02:17 -0400

    Update header div structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 export default function Header() {
     return ( 
         <header>
-            <h1>Luhn Algorithm Validator</h1>
-            <p> a checksum formula for numbers/digits used with credit card or administrative numbers.</p>
+            <div>
+                <h1>Luhn Algorithm Validator</h1>
+                <p> a checksum formula for numbers/digits used with credit card or administrative numbers.</p>
+            </div>
         </header> 
      );
 }
